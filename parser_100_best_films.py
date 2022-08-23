5702d4b797103ab1949f5c9a5f0cbe965118ec8b
kachus <90555926+kachus@users.noreply.github.com>
2022-08-23 19:30:00 +0400

    Update parser_100_best_films.py

--- parser_100_best_films.py
+++ parser_100_best_films.py
@@ -15,13 +15,8 @@ def parse(html_text):
     name = soup.findAll(attrs={"class": "_title_1p2xe_9 _titleNoCredit_1p2xe_197"})
     '''''
     name = soup.findAll(attrs={"class": "_title_1p2xe_9"})
-
-
     genre = soup.findAll(attrs={"class":"_text_163gl_28"})
     description = soup.findAll('div', class_ = "_summary_1p2xe_21")
-    print(len(name))
-
-
     for i in range(len(name)):
         film_name = name[i].text.split('.')[1]
         film_description = description[i].text
@@ -36,12 +31,6 @@ def parse(html_text):
 def chart(header, data):
     df = pd.DataFrame(data, columns = header)
     df.to_csv('/Users/Evgenia/Desktop/timeout_chart.csv')
-
-
-
-
-
-
 
 
 
